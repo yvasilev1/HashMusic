2641ee789b59632d3f9555f0c871de75db4ab36e
Ctrsewell-DundeeUniversity <ctrsewell@dundee.ac.uk>
2015-12-03 23:40:01 +0000

    Music Hash Tags fully functional.

---
+++ src/main/java/Servlets/PlayHashed.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import Models.HashTags;
+import com.datastax.driver.core.Cluster;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uk.ac.dundee.computing.aec.HashMusic.lib.CassandraHosts;
+
+/**
+ *
+ * @author Connor
+ */
+@WebServlet(name = "PlayHashed", urlPatterns = {"/PlayHashed"})
+public class PlayHashed extends HttpServlet {
+
+     Cluster cluster = null;
+
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+        cluster = CassandraHosts.getCluster();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        String hash = request.getParameter("id");
+        HttpSession session = request.getSession();
+        
+        HashTags ht = new HashTags();
+        ht.setCluster(cluster);
+        
+        java.util.UUID userID = (java.util.UUID)session.getAttribute("userID");
+        
+        java.util.UUID songID = ht.getHashSongID(hash, userID);
+    
+        response.sendRedirect("PlaySong?id="+songID);
+        
+    }
+
+ 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
